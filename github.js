a239df5330b54c6fd721735d20fe545fa775ca2c
sebastiand88 <68199815+sebastiand88@users.noreply.github.com>
2021-04-01 14:24:44 +0100

    Add files via upload
    first commit

---
+++ github.js
@@ -0,0 +1,26 @@
+class Github {
+  constructor() {
+    this.client_id = "9c81d16e85961b1168d7";
+    this.client_secret = "4c2341da4b6fad5629d549966165da78e53dc5b6";
+    this.repos_count = 5;
+    this.repos_sort = "created: asc";
+  }
+
+  async getUser(user) {
+    const profileResponse = await fetch(
+      `https://api.github.com/users/${user}?client_id=${this.client_id}&client_secret=${this.client_secret}`
+    );
+
+    const repoResponse = await fetch(
+      `https://api.github.com/users/${user}/repos?per_page=${this.repos_count}&sort=${this.repos_sort}&client_id=${this.client_id}&client_secret=${this.client_secret}`
+    );
+
+    const profile = await profileResponse.json();
+    const repos = await repoResponse.json();
+
+    return {
+      profile,
+      repos,
+    };
+  }
+}
